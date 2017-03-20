916034bb18d4e447168a67a7232954bfcb05328c
Matthew Schacht <mschacht94@comcast.net>
2017-03-19 18:25:50 -0700

    Emergency Task draft
    Basic functionality for Emergency task. Created structs and enums for queue messages (to be revised when integrated).

--- CST347-Elevator.X/EmergencyTasks.h
+++ CST347-Elevator.X/EmergencyTasks.h
@@ -1,5 +1,30 @@
 
-#ifndef _EMERGENCYTASKS_H    
-#define _EMERGENCYTASKS_H
+#ifndef EMERGENCYTASKS_H    
+#define EMERGENCYTASKS_H
 
-#endif /* _EMERGENCYTASKS_H */
+#include <stdint.h>
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "task.h"
+
+enum ec_floor {GD, P1, P2};
+enum dc_door {CLOSE, OPEN};
+
+typedef struct EMR_TASK_PARAMETERS{
+    QueueHandle_t emr_uart_rx_queue;
+    QueueHandle_t emr_ec_queue;     //Queue to Elevator Control task
+} emr_task_parameters;
+
+typedef struct EC_QUEUE_MESSAGE{
+    enum ec_floor ec_floor_request;     //Floor to go to
+    enum dc_door dc_door_command;       //Open or close door
+    uint8_t ec_priority;                //Priority of command (higher number = higher priority)
+} ec_queue_message;
+
+typedef struct URX_QUEUE_MESSAGE{
+    char command;
+} urx_queue_message;
+
+void emr_EmergencyTask(void *pvParameters);
+
+#endif
